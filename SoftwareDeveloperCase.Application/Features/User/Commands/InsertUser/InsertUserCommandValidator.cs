e5a561c226b36f1eb9506a36d53f1102fd1294d0
Pablo Martin <pablo.martin@ideaconsulting.es>
2022-10-31 15:58:38 +0100

    Bug fixes

--- SoftwareDeveloperCase.Application/Features/User/Commands/InsertUser/InsertUserCommandValidator.cs
+++ SoftwareDeveloperCase.Application/Features/User/Commands/InsertUser/InsertUserCommandValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using SoftwareDeveloperCase.Application.Contracts.Persistence;
+using System.Net.Mail;
 
 namespace SoftwareDeveloperCase.Application.Features.User.Commands.InsertUser
 {
@@ -18,19 +19,27 @@ namespace SoftwareDeveloperCase.Application.Features.User.Commands.InsertUser
             RuleFor(x => x.Email)
                 .NotEmpty().WithMessage("{PropertyName} cannot be empty")
                 .NotNull().WithMessage("{PropertyName} cannot be null")
-                .Must(NotExistingEmail).WithMessage("{PropertyName} already registered."); ;
+                .MustAsync(NotExistingEmail).WithMessage("{PropertyName} already registered."); ;
 
             RuleFor(x => x.Password)
                 .NotEmpty().WithMessage("{PropertyName} cannot be empty")
                 .NotNull().WithMessage("{PropertyName} cannot be null");
         }
 
-        private bool NotExistingEmail(string emailAddress)
+        private async Task<bool> NotExistingEmail(string emailAddress, CancellationToken cancellationToken)
         {
-            return !_unitOfWork.UserRepository
-                .GetAsync(u => u.Email.Equals(emailAddress))
-                .Result
-                .Any();
+            var users = await _unitOfWork.UserRepository
+                .GetAsync(u => u.Email!.Equals(emailAddress));
+
+            return !users.Any();
         }
+
+        //private async Task<bool> NotExistingEmail(string emailAddress)
+        //{
+        //    var users = await _unitOfWork.UserRepository
+        //        .GetAsync(u => u.Email!.Equals(emailAddress));
+
+        //    return !users.Any();
+        //}
     }
 }
